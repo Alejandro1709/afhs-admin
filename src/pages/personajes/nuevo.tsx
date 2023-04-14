f5d85c65f892125cb501fbec1e9ab9981c5247db
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-04-14 12:44:44 -0500

    Add: Form state

--- src/pages/personajes/nuevo.tsx
+++ src/pages/personajes/nuevo.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Layout from '@/components/Layout'
 import useAuth from '@/hooks/useAuth'
 import Link from 'next/link'
@@ -6,38 +7,65 @@ function NewCharacterPage() {
 
   const { user } = useAuth()
 
+  const [formData, setFormData] = useState({
+    name: '',
+    actor: '',
+    image: '',
+    birthdate: '',
+    work: '',
+    otherWork: '',
+    status: '',
+    gender: '',
+    height: '',
+    nicknames: '',
+    catchphrases: '',
+  })
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    console.log(formData)
+  }
+
   return (
     <Layout title='AFHS | Nuevo'>
       <section className='flex flex-col gap-2 mt-4'>
         <h1 className='text-3xl'>Nuevo personaje</h1>
-        <form className='flex flex-col gap-2'>
+        <form className='flex flex-col gap-2' onSubmit={handleSubmit}>
           <div className="flex flex-col gap-1">
             <label htmlFor="name">(*) Nombre:</label>
-            <input className='p-2 border' type="text" name="name" id="name" placeholder='Joel Gonzales' required />
+            <input className='p-2 border' type="text" name="name" id="name" placeholder='Joel Gonzales' required value={formData.name} onChange={handleChange} />
           </div>
           <div className="flex flex-col gap-1">
             <label htmlFor="actor">(*) Actor:</label>
-            <input className='p-2 border' type="text" name="actor" id="actor" placeholder='Erick Elera' required />
+            <input className='p-2 border' type="text" name="actor" id="actor" placeholder='Erick Elera' required value={formData.actor} onChange={handleChange} />
           </div>
           <div className="flex flex-col gap-1">
             <label htmlFor="image">URL de la imágen:</label>
-            <input className='p-2 border' type="text" name="actor" id="actor" placeholder='https://...' required />
+            <input className='p-2 border' type="text" name="image" id="image" placeholder='https://...' value={formData.image} onChange={handleChange} />
           </div>
           <div className="flex flex-col gap-1">
             <label htmlFor="birthdate">Fecha de nacimiento:</label>
-            <input className='p-2 border' type="text" name="birthdate" id="birthdate" placeholder='Junio 18, 1978' required />
+            <input className='p-2 border' type="text" name="birthdate" id="birthdate" placeholder='Junio 18, 1978' value={formData.birthdate} onChange={handleChange} />
           </div>
           <div className="flex flex-col gap-1">
             <label htmlFor="work">Ocupación:</label>
-            <input className='p-2 border' type="text" name="work" id="work" placeholder='Compositor,Mototaxista' required />
+            <input className='p-2 border' type="text" name="work" id="work" placeholder='Compositor,Mototaxista' value={formData.work} onChange={handleChange} />
           </div>
           <div className="flex flex-col gap-1">
             <label htmlFor="otherWork">Otras Ocupaciones:</label>
-            <input className='p-2 border' type="text" name="otherWork" id="otherWork" placeholder='Mecánico' required />
+            <input className='p-2 border' type="text" name="otherWork" id="otherWork" placeholder='Mecánico' value={formData.otherWork} onChange={handleChange} />
           </div>
           <div className="flex flex-col gap-1">
             <label htmlFor="status">(*) Estado:</label>
-            <select className='p-2 border' name="status" id="status" required>
+            <select className='p-2 border' name="status" id="status" required value={formData.status} onChange={handleChange}>
               <option value="" hidden defaultValue="">-- Seleccionar --</option>
               <option value="VIVO">Vivo</option>
               <option value="MUERTO">Muerto</option>
@@ -48,7 +76,7 @@ function NewCharacterPage() {
           </div>
           <div className="flex flex-col gap-1">
             <label htmlFor="gender">(*) Género:</label>
-            <select className='p-2 border' name="gender" id="gender" required>
+            <select className='p-2 border' name="gender" id="gender" required value={formData.gender} onChange={handleChange}>
               <option value="" hidden defaultValue="">-- Seleccionar --</option>
               <option value="HOMBRE">Hombre</option>
               <option value="MUJER">Mujer</option>
@@ -56,15 +84,15 @@ function NewCharacterPage() {
           </div>
           <div className="flex flex-col gap-1">
             <label htmlFor="height">Altura (m):</label>
-            <input className='p-2 border' type="text" name="height" id="height" placeholder='167' required />
+            <input className='p-2 border' type="text" name="height" id="height" placeholder='167' required value={formData.height} onChange={handleChange} />
           </div>
           <div className="flex flex-col gap-1">
             <label htmlFor="nicknames">Apodos:</label>
-            <input className='p-2 border' type="text" name="nicknames" id="nicknames" placeholder='Niño con cara de pez,La Leyenda' required />
+            <input className='p-2 border' type="text" name="nicknames" id="nicknames" placeholder='Niño con cara de pez,La Leyenda' required value={formData.nicknames} onChange={handleChange} />
           </div>
           <div className="flex flex-col gap-1">
             <label htmlFor="catchphrases">Frases:</label>
-            <input className='p-2 border' type="text" name="catchphrases" id="catchphrases" placeholder='Cua cua cua cuan,Superame pues,Ah que lindo' required />
+            <input className='p-2 border' type="text" name="catchphrases" id="catchphrases" placeholder='Cua cua cua cuan,Superame pues,Ah que lindo' required value={formData.catchphrases} onChange={handleChange} />
           </div>
           <div className="flex flex-col gap-1">
             <label htmlFor="autor">Autor:</label>
